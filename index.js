9a030d248f1ae01adb70970cef78330fbda65ad4
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-04 14:47:17 +0200

    Code label
    Added form label float up
    Added feature - label changes color to green if input is valid

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const submitButton = document.querySelector(".submit-button");
 const submitButtonText = document.querySelector(".submit-button-text");
 
 submitButton.onclick = () => {
-  submitButtonText.innerHTML = "Thanks";
+  submitButtonText.innerHTML = "Thank you";
   submitButton.classList.add("active");
 };
