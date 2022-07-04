4093ad51142519894837326b12c26b0eefc08414
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-07-04 16:39:27 +0200

    Change animated btn to normal one
    I deleted index.js (previous animated button settings)
    beacouse page redirects to new page after submit
    I deleted previous button styles from style.scss

--- index.js
+++
@@ -1,7 +0,0 @@
-const submitButton = document.querySelector(".submit-button");
-const submitButtonText = document.querySelector(".submit-button-text");
-
-submitButton.onclick = () => {
-  submitButtonText.innerHTML = "Thank you";
-  submitButton.classList.add("active");
-};
